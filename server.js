dde2ec57b2e5fd07cbc740cdacae25ed100a410c
rabhi alqadi <rabhialqadi22@gmail.com>
2023-12-17 22:19:16 -0800

    magic 8 ball app created

--- server.js
+++ server.js
@@ -32,3 +32,20 @@ app.get('/tip/:total/:tipPercentage', (req, res) => {
   const tipAmount = total * (tipPercentage / 100);
   res.send(`Your tip amount is: $${tipAmount.toFixed(2)}`);
 });
+
+app.get('/magic', (req, res) => {
+  res.send (`<h1>Magic Eight Ball</h1>`)
+})
+
+app.get('/magic/:question', (req, res) => {
+
+  const responses = ["It is certain", "It is decidedly so", "Without a doubt", "Yes definitely","You may rely on it", "As I see it yes", "Most likely", "Outlook good","Yes", "Signs point to yes", "Reply hazy try again", "Ask again later","Better not tell you now", "Cannot predict now", 
+  "Concentrate and ask again","Don't count on it", "My reply is no", "My sources say no","Outlook not so good", "Very doubtful"];
+
+  randPos = Math.floor(Math.random() * responses.length)
+  const question = req.params.question;
+  const answer =  responses[randPos];
+
+  res.send (` <h1>${question}?<h1>
+  <h2>Magic Eight Ball says: ${answer}</h2>`)
+})
